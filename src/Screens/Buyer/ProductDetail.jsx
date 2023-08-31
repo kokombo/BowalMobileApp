7ef3c3850807399829186c83ce08299a85f21081
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-08-31 23:56:40 +0100

    cart functionality

--- src/Screens/Buyer/ProductDetail.jsx
+++ src/Screens/Buyer/ProductDetail.jsx
@@ -8,8 +8,10 @@ import {
 } from './Components';
 import database from '@react-native-firebase/database';
 import {useSelector} from 'react-redux';
+import {useState} from 'react';
 
 const ProductDetail = ({route}) => {
+  const [accountType, setAccountType] = useState('');
   const {data} = route.params;
   const images = data?.productImages;
   const description = data?.productDescription;
@@ -18,26 +20,27 @@ const ProductDetail = ({route}) => {
 
   let quantity;
   let buttonContainer;
-
   //Referencing the database to check user's account type. If the user is a vendor, seleting number of products to cart and cart button will not show on the product details page. Only a buyer have access to the features.
   const ref = database().ref(`users/${user.uid}`);
   ref.on('value', snapshot => {
     const accountType = snapshot.child('accountType').val();
-    if (accountType === 'buyer') {
-      quantity = <ProductQuantity />;
-      buttonContainer = (
-        <View style={styles.buttons_container}>
-          <TouchableOpacity style={[styles.chat_button, styles.button]}>
-            <Text style={styles.chat_text}>chat</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={[styles.cart_button, , styles.button]}>
-            <Text style={styles.cart_text}>Add to cart</Text>
-          </TouchableOpacity>
-        </View>
-      );
-    }
+    setAccountType(accountType);
   });
+
+  if (accountType === 'buyer') {
+    quantity = <ProductQuantity />;
+    buttonContainer = (
+      <View style={styles.buttons_container}>
+        <TouchableOpacity style={[styles.chat_button, styles.button]}>
+          <Text style={styles.chat_text}>chat</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.cart_button, , styles.button]}>
+          <Text style={styles.cart_text}>Add to cart</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.body}>
